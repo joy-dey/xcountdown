47c8caf9a728c2867d66d7ba436fab367e0eba52
Joy Dey <joy.dey@outlook.com>
2024-12-11 10:35:29 +0530

    feat: add meta title and description for improved SEO

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,9 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+  <title>ChimeInTime | XLayer Technologies Employee Holiday Tracker</title>
+  <meta name="description" content="Counting down to your next break? ChimeInTime has got you covered! Track holidays, see what’s coming up, and plan your days off like a boss. Your ultimate holiday sidekick at XLayer Technologies!" />
+</svelte:head>
+
 {@render children()}
