afdafd619e6df0a6b0c9388ecb1db6956cbef05a
Joy Dey <joy.dey@outlook.com>
2024-12-11 10:22:43 +0530

    refractor(ui): Updated the design for past holidays

--- src/components/button-card.svelte
+++ src/components/button-card.svelte
@@ -7,7 +7,7 @@
 	let isPast = new Date(date) < new Date();
 </script>
 
-<button class="{styles} min-h-28 relative cursor-pointer rounded-xl p-2 text-left flex items-start flex-col gap-1 focus:ring-1 focus:outline-0 {isPast ? 'opacity-30 pointer-events-none cursor-not-allowed select-none': ''}" disabled="{isPast}" on:click>
+<button class="{styles} min-h-28 relative cursor-pointer rounded-xl p-2 text-left flex items-start flex-col gap-1 focus:ring-1 focus:outline-0 {isPast ? 'grayscale opacity-50 pointer-events-none cursor-not-allowed select-none': ''}" disabled="{isPast}" on:click>
 	<p class="text-lg font-medium leading-tight">{title}</p>
 	<small>{date}</small>
 	<span class="absolute text-6xl font-bold opacity-5 right-0 bottom-0">{index}</span>
