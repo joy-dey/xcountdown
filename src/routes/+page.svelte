3ec7512535ac91ced76f212631b73cf67d96b1c0
Joy Dey <joy.dey@outlook.com>
2024-12-11 10:22:22 +0530

    refractor(ui): Added Festival illustration for christmas

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,32 +3,43 @@
 	import { holidays, getNearestHoliday, getCountdownToDate } from '$lib/holidays';
 	import { onDestroy } from 'svelte';
 
-    $: holiday = getNearestHoliday(holidays);
+	$: holiday = getNearestHoliday(holidays);
 
 	$: countDown = getCountdownToDate(holiday.date);
 
 	const interval = setInterval(() => {
-		countDown = getCountdownToDate(holiday.date)
+		countDown = getCountdownToDate(holiday.date);
 	}, 1000);
 
 	onDestroy(() => clearInterval(interval));
-
-
 </script>
 
-<div class="container grid grid-cols-6 p-10 gap-8">
-	<div class="col-span-6 lg:col-span-2 rounded-2xl p-4 flex flex-col justify-end {holiday.style}">
-        <h1 class="text-4xl font-semibold uppercase">{holiday.name}</h1>
-		<p class="mt-4">Coming up in 
-			<span class="font-black text-5xl block uppercase">{countDown.days} Days</span>
-			<span class="italic text-lg">{countDown.hours} hours {countDown.minutes} minutes & {countDown.seconds} seconds</span>
-			<!-- d {countDown.hours} h {countDown.minutes} m {countDown.seconds} s -->
-		</p>
-    </div>
+<div class="grid grid-cols-6 gap-8 p-4 md:p-8 lg:p-10">
+	<div
+		class="col-span-6 w-full flex flex-col gap-16 md:gap-0 md:justify-between rounded-2xl lg:col-span-2 {holiday.style}"
+	>
+		<div class="group p-4">
+			<h1 class="text-4xl font-semibold uppercase">{holiday.name}</h1>
+			<p class="mt-4">
+				Coming up in
+				<span class="block text-5xl font-black uppercase">{countDown.days} Days</span>
+				<span class="text-lg italic"
+					>{countDown.hours} hours {countDown.minutes} minutes & {countDown.seconds} seconds</span
+				>
+			</p>
+		</div>
+		<img src="/images/pana.svg" alt="" />
+	</div>
 	<div class="col-span-6 lg:col-span-4">
 		<div class="card-grid grid grid-cols-4 gap-4">
 			{#each holidays as { serial, name, date, style }}
-				<Card title={name} {date} styles={style} index={serial} on:click="{() => holiday = {name, date, style}}"></Card>
+				<Card
+					title={name}
+					{date}
+					styles={style}
+					index={serial}
+					on:click={() => (holiday = { name, date, style })}
+				></Card>
 			{/each}
 		</div>
 	</div>
