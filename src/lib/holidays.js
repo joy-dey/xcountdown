229f5e0c73361405bdf9d9f13dbd6fcae9dbf67e
Joy Dey <joy.dey@outlook.com>
2025-01-03 17:19:01 +0530

    refractor(holidays): update holiday names and dates for 2025

--- src/lib/holidays.js
+++ src/lib/holidays.js
@@ -2,68 +2,74 @@ export 	const holidays = [
     {
         serial: '01',
         name: 'New Year',
-        date: 'Jan 01, 2024',
+        date: 'Jan 01, 2025',
         style: "bg-sky-100 text-sky-800 ring-sky-500  rounded col-span-4 md:col-span-2"
     },
     {
         serial: '02',
-        name: 'Makar Sankranti',
-        date: 'Jan 14, 2024',
+        name: 'Holi',
+        date: 'Mar 14, 2025',
         style: "bg-amber-100 text-amber-800 ring-amber-500 rounded col-span-4 md:col-span-1"
     },
     {
         serial: '03',
-        name: 'Republic Day',
-        date: 'Jan 26, 2024',
+        name: 'Ramzan',
+        date: 'Mar 31, 2025',
         style: "bg-orange-100 text-orange-800 ring-orange-500 rounded col-span-4 md:col-span-1"
     },
     {
         serial: '04',
-        name: 'Saraswati Puja',
-        date: 'Feb 14, 2024',
+        name: 'Bengali New Year',
+        date: 'Apr 14, 2025',
         style: "bg-violet-100 text-violet-800 ring-violet-500 rounded col-span-4 md:col-span-1"
     },
     {
         serial: '05',
-        name: 'Bengali New Year',
-        date: 'Apr 14, 2024',
+        name: 'Independence Day',
+        date: 'Aug 15, 2025',
         style: "bg-pink-100 text-pink-800 ring-pink-500 rounded col-span-4 md:col-span-1"
     },
     {
         serial: '06',
-        name: 'Independence Day',
-        date: 'Aug 15, 2024',
+        name: 'Maha Saptami',
+        date: 'Sep 29, 2025',
         style: "bg-green-100 text-green-800 ring-green-500 rounded col-span-4 md:col-span-2"
     },
     {
         serial: '07',
-        name: 'Gandhi Jayanti',
-        date: 'Oct 02, 2024',
+        name: 'Maha Ashtami',
+        date: 'Sep 30, 2025',
         style: "bg-gray-100 text-gray-800 ring-gray-500 rounded col-span-4 md:col-span-1"
     },
     {
         serial: '08',
-        name: 'Maha Saptami',
-        date: 'Oct 10, 2024',
+        name: 'Maha Navami',
+        date: 'Oct 01, 2025',
         style: "bg-indigo-100 text-indigo-800 ring-indigo-500 rounded col-span-4 md:col-span-1"
     },
     {
         serial: '09',
-        name: 'Maha Ashtami',
-        date: 'Oct 11, 2024',
+        name: 'Vijaya Dashami / Gandhi Jayanti',
+        date: 'Oct 02, 2025',
         style: "bg-purple-100 text-purple-800 ring-purple-500 rounded col-span-4 md:col-span-2 md:row-span-2"
     },
     {
         serial: '10',
-        name: 'Diwali',
-        date: 'Nov 01, 2024',
+        name: 'Laxmi Puja',
+        date: 'Oct 06, 2025',
         style: "bg-teal-100 text-teal-800 ring-teal-500 rounded col-span-4 md:col-span-1"
     },
     {
         serial: '11',
-        name: 'Christmas Day',
-        date: 'Dec 25, 2024',
+        name: 'Diwali',
+        date: 'Oct 21, 2025',
         style: "bg-red-100 text-red-800 ring-red-500 rounded col-span-4 md:col-span-1"
+    },
+    {
+        serial: '12',
+        name: 'Christmas',
+        date: 'Dec 25, 2025',
+        style: "bg-sky-100 text-sky-800 ring-sky-500  rounded col-span-4"
     }
 ];
 
