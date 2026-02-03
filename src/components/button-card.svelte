261428deb4d058f3c878d7e6db738d031e6bbcee
Joy Dey <joy.dey@outlook.com>
2026-02-03 10:10:55 +0530

    Modify button to show date with current day
    Updated the button display to include the current day of the week based on the date.

--- src/components/button-card.svelte
+++ src/components/button-card.svelte
@@ -5,10 +5,12 @@
 	export let index;
 
 	let isPast = new Date(date) < new Date();
+	let days = ['Sun', 'Mon','Tue','Wed','Thu','Fri','Sat'];
+	let currentDay = days[new Date(date).getDay()];
 </script>
 
 <button class="{styles} min-h-28 relative cursor-pointer rounded-xl p-2 text-left flex items-start flex-col gap-1 focus:ring-1 focus:outline-0 {isPast ? 'grayscale opacity-50 pointer-events-none cursor-not-allowed select-none': ''}" disabled="{isPast}" on:click>
 	<p class="text-lg font-medium leading-tight">{title}</p>
-	<small>{date}</small>
+	<small>{date}-{currentDay}</small>
 	<span class="absolute text-6xl font-bold opacity-5 right-0 bottom-0">{index}</span>
 </button>
